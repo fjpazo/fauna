42689e79ab3e13e3c60a105a08570d23cd86f229
fjpazo <fjpazo@outlook.es>
2025-04-04 21:39:05 +0200

    First version

---
+++ listaAnimales.py
@@ -0,0 +1,11 @@
+class ListaAnimales:
+    def __init__(self):
+        self.animales = []
+
+    def agregar_animal(self, animal):
+        self.animales.append(animal)
+
+    def mostrar_animales(self):
+        print("Lista de Animales:")
+        for i, animal in enumerate(self.animales, 1):
+            print(f"{i}. Nombre: {animal.nombre}, Edad: {animal.edad}, Especie: {animal.especie}")
---
+++ main.py
@@ -0,0 +1,9 @@
+﻿from listaAnimales import ListaAnimales
+from fauna import Fauna
+
+def main():
+    listado = ListaAnimales()
+    Fauna.menu(listado)
+
+if __name__ == "__main__":
+    main()
